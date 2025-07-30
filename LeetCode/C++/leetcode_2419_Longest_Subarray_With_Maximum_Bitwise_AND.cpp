6218071f55a2390f3f89043f471500732c9f3be8
Anshuman Raj <84335613+its-Anshuman@users.noreply.github.com>
2025-07-30 07:10:25 +0530

    Create leetcode_2419_Longest_Subarray_With_Maximum_Bitwise_AND.cpp

---
+++ LeetCode/C++/leetcode_2419_Longest_Subarray_With_Maximum_Bitwise_AND.cpp
@@ -0,0 +1,23 @@
+# LeetCode POTD - Longest Subarray With Maximum Bitwise AND
+# Date: 2025-07-30
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int maxi = *max_element(nums.begin(),nums.end());
+        int res = 1;
+        int currLen = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==maxi){
+                currLen++;
+            }else{
+                res = max(res,currLen);
+                currLen = 0;
+            }
+
+        }
+        res = max(res,currLen);
+        return res;
+    }
+};
